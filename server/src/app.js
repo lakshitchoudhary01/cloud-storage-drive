27823dffd4e4d8e66d9e1675a22c4b3d3e1f36fa
Lakshit Choudhary <choudharylakshit2@gmail.com>
2026-08-01 22:54:59 +0530

    Configure Express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,26 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-// Test Route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Cloud Storage Drive API is Running 🚀");
+  res.status(200).json({
+    success: true,
+    message: "Cloud Storage Drive API is running 🚀",
+    version: "1.0.0",
+  });
 });
 
 export default app;
